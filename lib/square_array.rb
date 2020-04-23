aa60504b069c578cb0d815fb3dc0121ba3d7f40d
Senada Kadric <skadric@fordham.edu>
2020-04-23 12:24:59 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,11 +1,13 @@
 def square_array(array)
   square_array = []
 
+  #initial solution w/ iterating and looping
   #count = 0
   #while count < array.length
     #square_array << array[count] ** 2
     #count += 1
   #end
+  
   array.length.times { |index| square_array << array[index] ** 2 }
 
   square_array
