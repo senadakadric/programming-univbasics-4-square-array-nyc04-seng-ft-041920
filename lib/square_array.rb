6a4f4dfb15ec956158cfd0fd825e75b4be356a70
Senada Kadric <skadric@fordham.edu>
2020-04-23 12:24:28 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -6,7 +6,7 @@ def square_array(array)
     #square_array << array[count] ** 2
     #count += 1
   #end
-  array.length.times { |index| squared_array << array[index] ** 2 }
+  array.length.times { |index| square_array << array[index] ** 2 }
 
   square_array
 end
